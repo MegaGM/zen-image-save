3a64845ffb2d5ecb628a235d9cc7a810fb43162f
Mega <Aza.Noriega@gmail.com>
2018-01-26 21:23:14 +0500

    change prompt method to overwrite for filenames

--- js/bg.js
+++ js/bg.js
@@ -4,23 +4,20 @@
 chrome.downloads.onDeterminingFilename.addListener(function (item, suggest) {
   suggest({
     filename: item.filename,
-    conflictAction: 'prompt'
+    conflictAction: 'overwrite',
   })
-  console.log('ondeterm');
 });
 
 /**
  * Listen to inject.js
  */
 chrome.runtime.onMessage.addListener(function (req, sender, res) {
-  chrome.tabs.executeScript({
-    code: 'console.log("addd")'
-  });
-  console.log('backend was cda');
+  // chrome.tabs.executeScript({
+  //   code: 'console.log("addd")'
+  // });
   if ('download' !== req.key) return res({});
 
   chrome.downloads.download({ url: req.url }, function () {
-    console.log(arguments[0]);
     return res({});
   });
 });
